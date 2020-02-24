cebf6ec7f771edbc05aac7bde8cc2b5db1f7e3b1
tusharubale20 <59358405+tusharubale20@users.noreply.github.com>
2020-02-24 11:40:51 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello Java world");
+		System.out.println("Hello Java world edited for Jenkins SCM poll");
 	}
 }
